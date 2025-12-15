58d716403bd9b367649380787c0039c1e322aa5d
techlx1 <31870429+techlx1@users.noreply.github.com>
2025-12-15 12:58:27 -0400

    app suggestion

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,8 +1,13 @@
 import { getAppVersion } from "../controllers/appVersionController.js";
 import express from "express";
+import { verifyToken } from '../middleware/authMiddleware.js';
+import { submitSuggestion } from '../controllers/settingsController.js';
+
+
 
 const router = express.Router();
 
 router.get("/version", getAppVersion);
+router.post('/suggestions', verifyToken, submitSuggestion);
 
 export default router;
